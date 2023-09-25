2952e1e18dc8dcf132450b8da8e0f4f847aad11b
Mehedihasan444 <mehedihasan67705251@gmail.com>
2023-09-26 03:23:42 +0600

    adding see more and less button on doantion page

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -5,6 +5,7 @@ import Donate_card from "../../Components/Donate_card/Donate_card";
 const Donation = () => {
   const [donation, setDonation] = useState([]);
   const [noFound, setNoFound] = useState("");
+  const [isShow,setIsShow]=useState(false)
   useEffect(() => {
     const donations = JSON.parse(localStorage.getItem("donations"));
     if (donations) {
@@ -15,10 +16,6 @@ const Donation = () => {
     }
   }, []);
 
-
-  if (donation.length>4) {
-    donation.slice(0,4)
-  }
   return (
     <div>
       
@@ -26,13 +23,17 @@ const Donation = () => {
         <p className="h-[80vh] flex justify-center items-center">{noFound}</p>
       ) : (
         <div className="">
-          <div className="max-w-7xl mx-auto pt-20 grid grid-cols-2 gap-5 ">
-            {donation.map((donate) => (
+          <div className="max-w-7xl mx-auto pt-24 grid grid-cols-2 gap-5 ">
+            {isShow?donation.map((donate) => (
+              <Donate_card key={donate.Id} donate={donate}></Donate_card>
+            )):
+            
+            donation.slice(0,4).map((donate) => (
               <Donate_card key={donate.Id} donate={donate}></Donate_card>
             ))}
           </div>
           <div className="flex justify-center items-center mt-10">
-        <button className="px-5 py-2 text-white rounded-md bg-green-500">See All</button>
+        <button className="px-5 py-2 text-white rounded-md bg-green-500" onClick={()=>setIsShow(!isShow)}>{isShow?"See less":"See All"}</button>
       </div>
         </div>
       )}
